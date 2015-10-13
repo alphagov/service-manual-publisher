68a7b949b5dc9b3e61c4472e8889d56002d4d0ed
Andrew Vos <andrew.vos@gmail.com>
2015-10-13 13:41:06 +0100

    DRY up GuideController

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -5,20 +5,7 @@ class GuidesController < ApplicationController
   def create
     guide = Guide.new
     guide.slug = params[:guide][:slug]
-
-    edition = Edition.new
-    [:title, :body].each do |a|
-      edition.send("#{a}=", params[:guide][:editions][a])
-    end
-
-    if params[:save_draft]
-      edition.state = "draft"
-    elsif params[:publish]
-      edition.state = "published"
-    end
-
-    guide.editions << edition
-
+    guide.editions << create_edition_from_params(params[:guide][:editions])
     guide.save!
   end
 
@@ -28,15 +15,24 @@ class GuidesController < ApplicationController
 
   def update
     guide = Guide.find(params[:id])
+    guide.editions << create_edition_from_params(params[:guide][:edition])
+  end
+
+  private
+
+  def create_edition_from_params hash
     edition = Edition.new
+
     [:title, :body].each do |a|
-      edition.send("#{a}=", params[:guide][:edition][a])
+      edition.send("#{a}=", hash[a])
     end
+
     if params[:save_draft]
-      edition.state = "draft"
+      edition.state = 'draft'
     elsif params[:publish]
-      edition.state = "published"
+      edition.state = 'published'
     end
-    guide.editions << edition
+
+    edition
   end
 end
