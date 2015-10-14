e06b571f006f7c885277449c9b1965633b3afa2f
Tadas Tamosauskas <tadas@pdfcv.com>
2015-10-14 11:55:08 +0100

    Fix a failing test
    The test had two issues
    
    1. latest_edition was nil
    
    Assigning an array of editions and referring to latest_edition in a callback
    did not work as these are two separate relationships. I believe callbacks are
    executed in DB transactions, therefore latest_edition was not persisted when
    it was being accessed. Assigning latest_edition directly solves this problem.
    
    2. Calls to publishing-api were not stubbed
    
    I've stubbed out the private #publish method on a guide as this test is not
    concerned with the publishing process.

--- spec/models/guide_spec.rb
+++ spec/models/guide_spec.rb
@@ -17,8 +17,9 @@ RSpec.describe Guide do
 
   describe "on create callbacks" do
     it "generates and sets content_id on create" do
+      allow_any_instance_of(Guide).to receive(:publish)
       edition = valid_edition(title: "something", state: "published")
-      guide = Guide.create!(slug: "/slug", content_id: nil, editions: [edition])
+      guide = Guide.create!(slug: "/slug", content_id: nil, latest_edition: edition)
       expect(guide.content_id).to be_present
     end
   end
