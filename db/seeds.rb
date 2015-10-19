2674cee820b96a93063b326f7af5e389f243cee5
Andrew Vos <andrew.vos@gmail.com>
2015-10-19 15:46:22 +0100

    Seed article data from old service manual repository
    This allows designers to easily get some data into the database, and
    start with designing the new pages.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+if Rails.env.development?
+  unless Dir.exist?("../government-service-design-manual")
+    print "Cloning repository..."
+    sh "cd ../ && git clone https://github.com/alphagov/government-service-design-manual"
+    puts " [done]"
+  end
+  objects = Dir.glob("../government-service-design-manual/service-manual/**/*.md")
+  .map do |path|
+    url = path.gsub("../government-service-design-manual", "")
+    url = url.gsub(".md", "")
+    url = url.gsub(/\/index$/, '')
+    {
+      path: path,
+      url: url,
+    }
+  end
+
+  # https://github.com/jekyll/jekyll/blob/2807b8a012ead8b8fe7ed30f1a8ad1f6f9de7ba4/lib/jekyll/document.rb
+  YAML_FRONT_MATTER_REGEXP = /\A(---\s*\n.*?\n?)^((---|\.\.\.)\s*$\n?)/m
+
+  objects.each do |object|
+    content = File.read(object[:path])
+    title = ""
+
+    if content =~ YAML_FRONT_MATTER_REGEXP
+      body = $POSTMATCH
+      data_file = YAML.load($1)
+      title = data_file["title"]
+      state = data_file["status"]
+
+      if Guide.find_by_slug(object[:url]).present?
+        next
+        puts "Ignoring '#{title}'"
+      end
+      puts "Creating '#{title}'"
+
+      next if body.blank?
+
+      edition = Edition.new(
+        title:           title,
+        state:           state.present? ? state : "draft",
+        phase:           "beta",
+        description:     "Description",
+        update_type:     "major",
+        body:            body,
+        publisher_title: Edition::PUBLISHERS.keys.first
+      )
+      guide = Guide.create!(slug: object[:url], content_id: nil, latest_edition: edition)
+      GuidePublisher.new(guide).publish!
+    end
+  end
+end
