f7f3abb0f37ef6667e3655e95bd9850665e72150
Tadas Tamosauskas <tadas@pdfcv.com>
2015-10-23 15:05:41 +0100

    Add a very basic guide history page
    This allows users to see who and when made editions to any given guide and the
    dates editions where published. Mostly for demo and development convenience
    purposes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   root 'guides#index'
 
-  resources :guides
+  resources :guides do
+    resources :editions, only: :index
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
