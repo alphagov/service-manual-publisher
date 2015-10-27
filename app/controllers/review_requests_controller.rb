16c22c628348ad576a30514e649806693ae244bc
Andrew Vos <andrew.vos@gmail.com>
2015-10-27 15:24:40 +0000

    Refactor ReviewRequestsController

--- app/controllers/review_requests_controller.rb
+++ app/controllers/review_requests_controller.rb
@@ -3,11 +3,9 @@ class ReviewRequestsController < ApplicationController
     review_request = ReviewRequest.create!
 
     guide = Guide.find(params[:review_request][:guide_id])
-    editions = guide.editions.where(review_request: nil)
-    editions.each do |edition|
-      edition.review_request = review_request
-      edition.save!
-    end
+    guide.editions
+      .where(review_request: nil)
+      .update_all(review_request_id: review_request)
 
     redirect_to root_path, notice: "Your review request has been created!"
   end
