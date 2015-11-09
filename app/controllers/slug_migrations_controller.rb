37f4651dae9430ba537e0b6b21958eec938c4c44
Andrew Vos <andrew.vos@gmail.com>
2015-11-09 15:40:22 +0000

    Allow filtering slugs by completed/incompleted
    Tried to find a quick way using javascript, but turns out this was
    easier.

--- app/controllers/slug_migrations_controller.rb
+++ app/controllers/slug_migrations_controller.rb
@@ -1,5 +1,8 @@
 class SlugMigrationsController < ApplicationController
   def index
     @migrations = SlugMigration.all
+    if params[:completed].present?
+      @migrations = @migrations.where(completed: params[:completed])
+    end
   end
 end
--- spec/features/slug_migration_spec.rb
+++ spec/features/slug_migration_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 require 'capybara/rails'
 
 RSpec.describe "Slug migration", type: :feature do
+  def expect_table_to_match_migrations migrations
+    within ".migrations .table" do
+      table_data = page.all("tr").map do |row|
+        row.all("td").map(&:text)
+      end
+
+      expect(table_data).to eq(migrations.map { |m| ["", m.slug, m.completed.to_s] })
+    end
+  end
+
   feature "with incomplete migrations" do
     before do
       @migrations = (1..3).map do |i|
-        SlugMigration.create(completed: false, slug: "/old/bar#{i}")
+        SlugMigration.create!(completed: false, slug: "/old/bar#{i}")
       end
     end
 
@@ -16,19 +26,38 @@ RSpec.describe "Slug migration", type: :feature do
         click_link "Manage Migrations"
       end
 
-      within ".migrations .table" do
-        table_data = page.all("tr").map do |row|
-          row.all("td").map(&:text)
-        end
-
-        expect(table_data).to eq(@migrations.map { |m| ["", m.slug, m.completed.to_s] })
-      end
+      expect_table_to_match_migrations @migrations
     end
   end
 
   feature "can filter migrations" do
-    it "based on complete"
-    it "based on incompleted"
+    before do
+      @incompleted = (1..2).map do |i|
+        SlugMigration.create!(completed: false, slug: "/old/bar#{i}")
+      end
+      @complete = (1..2).map do |i|
+        SlugMigration.create!(completed: true, slug: "/old/foo#{i}")
+      end
+
+      visit root_path
+      within "nav" do
+        click_link "Manage Migrations"
+      end
+    end
+
+    it "based on complete" do
+      within ".slug-filters" do
+        click_link "Filter by completed"
+      end
+      expect_table_to_match_migrations @complete
+    end
+
+    it "based on incompleted" do
+      within ".slug-filters" do
+        click_link "Filter by incompleted"
+      end
+      expect_table_to_match_migrations @incompleted
+    end
   end
 
   it "can complete a migration event for incompleted migrations"
