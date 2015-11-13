d6a8f81677925d7f4fb158aebd99d55d9d5ae3ec
Andrew Vos <andrew.vos@gmail.com>
2015-11-13 11:35:19 +0000

    Validation methods are private

--- app/models/slug_migration.rb
+++ app/models/slug_migration.rb
@@ -2,29 +2,33 @@ class SlugMigration < ActiveRecord::Base
   belongs_to :guide
 
   validates :slug, uniqueness: true
-  validate :guide_must_be_nil_or_published
-  validate :guide_cant_be_empty_when_migrating
-  validate :is_not_already_completed
+  validate(
+    :guide_must_be_nil_or_published,
+    :guide_cant_be_empty_when_migrating,
+    :is_not_already_completed,
+  )
 
   before_validation on: :create do |object|
     object.content_id = SecureRandom.uuid
   end
 
-  def guide_must_be_nil_or_published
-    if guide && !guide.editions.where(state: "published").any?
-      errors.add(:guide, "must have a published edition")
-    end
-  end
+  private
 
-  def guide_cant_be_empty_when_migrating
-    if completed? && guide.nil?
-      errors.add(:guide, "must not be blank")
+    def guide_must_be_nil_or_published
+      if guide && !guide.editions.where(state: "published").any?
+        errors.add(:guide, "must have a published edition")
+      end
     end
-  end
 
-  def is_not_already_completed
-    if completed_was
-      errors.add(:base, "must not already be completed")
+    def guide_cant_be_empty_when_migrating
+      if completed? && guide.nil?
+        errors.add(:guide, "must not be blank")
+      end
     end
-  end
+
+    def is_not_already_completed
+      if completed_was
+        errors.add(:base, "must not already be completed")
+      end
+    end
 end
