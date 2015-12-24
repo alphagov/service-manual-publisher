7750acef40749e34f4b4ea0c32e1eb68f648c475
Tadas Tamosauskas <tadas@pdfcv.com>
2015-12-24 10:49:39 +0000

    Notify users when there are unsaved changes
    Ask users to confirm their navigation away when the guides form has unsaved
    changes. It works by recording a checksum of the form values on page load, then
    checking if the checksum has changed before the user tries to navigate away.
    This is better than relying on 'change' events of form inputs because it takes
    into account the fact that users may change the values back to the previous
    ones.

---
+++ app/assets/javascripts/protect_form_data.js
@@ -0,0 +1,50 @@
+$(function(){
+  window.okToNavigateAway = false;
+
+  $("form.js-protect-data").each(function(_, form) {
+    var $form = $(form);
+    recordChecksum($form);
+    $form.on("click", ".js-ok-to-navigate-away", function(evt){
+      window.okToNavigateAway = true;
+    });
+  });
+
+  window.onbeforeunload = function() {
+    if (window.okToNavigateAway){
+      return undefined;
+    }
+
+    forms = $("form.js-protect-data");
+    for (var i = forms.length - 1; i >= 0; i--) {
+      var form = forms[i];
+      if (hasChanged($(form))) {
+        return "It looks like there are some unsaved changes in the form.\n\nLeaving the page will discard all unsaved changes.";
+      }
+    }
+  };
+
+  function hasChanged(form){
+    if (form) {
+      var currentChecksum = checksum(form.serialize()).toString();
+      var previousChecksum = form.attr("data-checksum");
+      return currentChecksum != previousChecksum;
+    }
+    return false;
+  }
+});
+
+function checksum(string) {
+  var hashValue = 0, i, chr, len;
+  if (string.length === 0) return hashValue;
+  for (i = 0, len = string.length; i < len; i++) {
+    chr   = string.charCodeAt(i);
+    hashValue  = ((hashValue << 5) - hashValue) + chr;
+    hashValue |= 0; // Convert to 32bit integer
+  }
+  return hashValue;
+}
+
+function recordChecksum(form) {
+  var checksumValue = checksum(form.serialize());
+  form.attr("data-checksum", checksumValue);
+}
