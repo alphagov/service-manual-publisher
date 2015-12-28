25c34bac55fd3f54c228a48947553f4e4f4d8c30
Tadas Tamosauskas <tadas@pdfcv.com>
2015-12-28 10:36:56 +0000

    Add default 'from' email address to NotificationMailer
    We'll need a 'from' address for sending notifications. We're not planning to
    parse reply emails so it has to be a DON'T REPLY address. Implementation
    inspired by Whitehall's DO NOT REPLY [1]
    
    [1] - https://github.com/alphagov/whitehall/blob/5631a1722e186b194f4f7bb1f53cd2eb56e48034/app/mailers/notifications.rb#L60-L69

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,15 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  def self.no_reply_email_address
+    name = "(Service Manual) DON'T REPLY"
+    if GovukAdminTemplate.environment_label !~ /production/i
+      name.prepend("[GOV.UK #{GovukAdminTemplate.environment_label}] ")
+    end
+
+    address = Mail::Address.new("inside-government@digital.cabinet-office.gov.uk")
+    address.display_name = name
+    address.format
+  end
+
+  default from: no_reply_email_address
   layout 'mailer'
 end
