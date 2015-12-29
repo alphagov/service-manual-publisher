e44c3480ad49c4d710303b3b317f0cde85dbbe14
Tadas Tamosauskas <tadas@pdfcv.com>
2015-12-29 11:38:39 +0000

    Show a custom search header based on search query parameters
    This feature might be useful for adding context when directly linking to a
    filtered page and help users detect any errors they might make when searching.

---
+++ app/presenters/search_header_presenter.rb
@@ -0,0 +1,40 @@
+class SearchHeaderPresenter
+  attr_reader :params, :current_user
+
+  def initialize(params, current_user)
+    @params = params
+    @current_user = current_user
+  end
+
+  def to_s
+    [ownership, state, "guides", match, published_by].compact.join(" ")
+  end
+
+  def search?
+    [:user, :state, :content_owner, :q].any? { |field| params[field].present? }
+  end
+
+  def ownership
+    if params[:user].to_i == current_user.id
+      "My"
+    elsif params[:user].present?
+      "#{User.find(params[:user]).name}'s"
+    else
+      "Everyone's"
+    end
+  end
+
+  def state
+    params[:state].to_s.humanize.downcase.presence
+  end
+
+  def match
+    "matching \"#{params[:q]}\"" if params[:q].present?
+  end
+
+  def published_by
+    if params[:content_owner].present?
+      "published by #{ContentOwner.find(params[:content_owner]).title}"
+    end
+  end
+end
