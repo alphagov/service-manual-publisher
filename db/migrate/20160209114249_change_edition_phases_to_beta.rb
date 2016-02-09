da53416396e3f27f9099ed14bf32cade15b70937
Andrew Vos <andrew.vos@gmail.com>
2016-02-09 11:45:38 +0000

    Change default phase from "alpha" to "beta"

---
+++ db/migrate/20160209114249_change_edition_phases_to_beta.rb
@@ -0,0 +1,6 @@
+class ChangeEditionPhasesToBeta < ActiveRecord::Migration
+  def change
+    change_column :editions, :phase, :text, default: 'beta'
+    execute "UPDATE editions SET phase='beta' WHERE phase='alpha'"
+  end
+end
