7e681a855219ba2ae89a4ca9d9770362c4665b02
Henry Turner <henry.turner@digital.cabinet-office.gov.uk>
2016-02-19 08:47:00 +0000

    Publisher#save_draft sends the content to the publishing api

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,11 +1,13 @@
 class Publisher
-  attr_reader :content_model
+  attr_reader :content_model, :publishing_api
 
-  def initialize(content_model:)
+  def initialize(content_model:, publishing_api: PUBLISHING_API)
     @content_model = content_model
+    @publishing_api = publishing_api
   end
 
   def save_draft
     content_model.save
+    publishing_api.put_content(content_model.content_id, {})
   end
 end
--- spec/models/publisher_spec.rb
+++ spec/models/publisher_spec.rb
@@ -1,11 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Publisher, '#save_draft' do
+
   it 'persists the content model' do
     guide = Generators.valid_guide
+    publishing_api = FakePublishingApi.new
 
-    Publisher.new(content_model: guide).save_draft
+    Publisher.new(content_model: guide, publishing_api: publishing_api).
+              save_draft
 
     expect(guide).to be_persisted
+  end
+
+  class FakePublishingApi
+    def put_content(content_id, data)
+    end
+  end
+
+  it 'sends the draft to the publishing api' do
+    guide = Generators.valid_guide
+    guide.save!
+    publishing_api = double(:publishing_api)
+
+    expect(publishing_api).to receive(:put_content).
+                              with(guide.content_id, {})
+
+    Publisher.new(content_model: guide, publishing_api: publishing_api).
+              save_draft
   end
 end
