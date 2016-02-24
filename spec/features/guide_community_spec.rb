96e05b080cae4d1891dfd3a1364d53ed5b2a52dc
Henry Turner <henry.turner@digital.cabinet-office.gov.uk>
2016-02-24 17:54:56 +0000

    Broken feature to create a guide community

---
+++ spec/features/guide_community_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Create a guide community', type: :feature do
+  it 'successfully creates a guide community' do
+    visit root_path
+    click_link "Create a Guide Community"
+
+    publishing_api = double(:publishing_api)
+    stub_const("PUBLISHING_API", publishing_api)
+    expect(publishing_api).to receive(:put_content)
+
+    fill_in 'Slug', with: '/service-manual/design-community'
+    fill_in "Guide description", with: "This acts as a test case"
+    fill_in "Guide title", with: "First Edition Title"
+    fill_in "Body", with: "## First Edition Title"
+    click_first_button "Save"
+
+    within ".alert" do
+      expect(page).to have_content('created')
+    end
+
+    # Reload the page to avoid any false positive assertions on
+    # the content of the fields
+    visit current_path
+
+    expect(page).to have_field('Slug', with: '/service-manual/design-community')
+    expect(page).to have_field('Guide description', with: 'This acts as a test case')
+    expect(page).to have_field('Guide title', with: 'First Edition Title')
+    expect(page).to have_field('Body', with: '## First Edition Title')
+  end
+end
