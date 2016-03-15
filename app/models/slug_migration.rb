1eb776cdba1bf399bf7ea356e30fda296c5eff6f
Henry Turner <henry.turner@digital.cabinet-office.gov.uk>
2016-03-15 13:43:49 +0000

    Remove the delete indexed document from slug migration tool
    We have discovered since that the old service manual search is rebuilt periodically. Items removed manually won't have a permanent effect.

--- app/models/slug_migration.rb
+++ app/models/slug_migration.rb
@@ -10,20 +10,6 @@ class SlugMigration < ActiveRecord::Base
     object.content_id = SecureRandom.uuid
   end
 
-  def has_search_document?
-    @search_client ||= GdsApi::Rummager.new(Plek.current.find('rummager'), disable_cache: true)
-    begin
-      @search_client.get_content!(slug)
-    rescue GdsApi::HTTPErrorResponse => e
-      false
-    end
-  end
-
-  def delete_search_document!
-    @search_client ||= GdsApi::Rummager.new(Plek.current.find('rummager'), disable_cache: true)
-    @search_client.delete_content!(slug)
-  end
-
   private
 
     def is_not_already_completed
