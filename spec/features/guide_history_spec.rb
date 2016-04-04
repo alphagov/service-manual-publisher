74466c5877f7e797ca20511e0cdd80adcf74d771
Henry Turner <henry.turner@digital.cabinet-office.gov.uk>
2016-04-04 12:58:50 +0000

    A broken feature spec for state changes in the guide history

--- spec/features/guide_history_spec.rb
+++ spec/features/guide_history_spec.rb
@@ -67,6 +67,22 @@ RSpec.describe "Guide history", type: :feature do
     expect(events.third).to include "What a great piece of writing"
   end
 
+  it "shows state changes" do
+    stub_publisher
+    create_guide_community
+    visit root_path
+    click_on "Create a Guide"
+    fill_out_new_guide_fields
+    click_first_button "Save"
+    click_first_button "Send for review"
+    click_first_button "Save"
+
+    click_on "Comments and history"
+
+    expect(events.third).to eq "Review requested by Stub User"
+    expect(events.fourth).to eq "Draft updated by Stub User"
+  end
+
   def events
     all(".event")
       .map(&:text)
