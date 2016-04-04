f2f076881e24ac35185494aa33ad47f47c9c7793
Andrew Vos <andrew.vos@gmail.com>
2016-04-04 11:40:42 +0100

    Remove the Actions column
    It's not really useful

--- app/helpers/guide_helper.rb
+++ app/helpers/guide_helper.rb
@@ -14,14 +14,6 @@ module GuideHelper
     content_tag :span, title, title: 'State', class: "label label-#{css_class}"
   end
 
-  def guide_action_button(guide)
-    title = {
-      "review_requested" => "Review guide",
-      "ready" => "Publish",
-    }[guide.latest_edition.state] || "Edit"
-    link_to title, edit_guide_path(guide), class: "btn btn-block btn-default btn-xs"
-  end
-
   def latest_editor_name(guide)
     guide.latest_edition.user.try(:name).to_s
   end
