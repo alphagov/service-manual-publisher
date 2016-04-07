763f633e98642d71a6cd48fb042a8541d3df518b
Andrew Vos <andrew.vos@gmail.com>
2016-04-07 11:28:05 +0100

    Fix tests that only pass on 6th April 2016

--- spec/features/guide_history_spec.rb
+++ spec/features/guide_history_spec.rb
@@ -1,29 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "Guide history", type: :feature do
+  include ActiveSupport::Testing::TimeHelpers
+
   it "shows a history of the latest edition" do
     stub_publisher
     create_guide_community
-    visit root_path
-    click_on "Create a Guide"
-    fill_out_new_guide_fields
-    click_first_button "Save"
 
-    click_on "Comments and history"
+    travel_to "2004-11-24 01:04:44".to_time do
+      visit root_path
+      click_on "Create a Guide"
+      fill_out_new_guide_fields
+      click_first_button "Save"
 
-    within ".open-edition" do
-      fill_in "Add new comment", with: "What a great piece of writing"
-      click_button "Save comment"
+      click_on "Comments and history"
+
+      within ".open-edition" do
+        fill_in "Add new comment", with: "What a great piece of writing"
+        click_button "Save comment"
+      end
+
+      click_first_button "Send for review"
     end
 
-    click_first_button "Send for review"
-
     click_on "Comments and history"
 
-    expect(events.first).to eq "06 April 2016 New draft created by Stub User"
-    expect(events.second).to eq "06 April 2016 Assigned to Stub User"
-    expect(events.third).to include "06 April 2016 Stub User What a great piece of writing"
-    expect(events.fourth).to eq "06 April 2016 Review requested by Stub User"
+    expect(events.first).to eq "24 November 2004 New draft created by Stub User"
+    expect(events.second).to eq "24 November 2004 Assigned to Stub User"
+    expect(events.third).to include "24 November 2004 Stub User What a great piece of writing"
+    expect(events.fourth).to eq "24 November 2004 Review requested by Stub User"
   end
 
   def stub_publisher
