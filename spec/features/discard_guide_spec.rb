b2c195b7e1697a296a7590f414255dc4509d525f
Andrew Vos <andrew.vos@gmail.com>
2016-04-11 13:59:53 +0100

    Don't show 'Discard draft' when latest edition is published

--- spec/features/discard_guide_spec.rb
+++ spec/features/discard_guide_spec.rb
@@ -2,19 +2,17 @@ require 'rails_helper'
 require 'capybara/rails'
 
 RSpec.describe "discarding guides", type: :feature do
-  let :guide do
-    create(
-      :guide,
-      editions: [
-        build(:draft_edition, body: "This is the first draft edition"),
-        build(:published_edition, body: "This is the published edition"),
-        build(:draft_edition, body: "This is the latest draft edition"),
-      ],
-    )
+  context "when the latest edition is published" do
+    it "does not allow discarding" do
+      guide = create(:published_guide)
+      visit edit_guide_path(guide)
+      expect(page).to_not have_button "Discard draft"
+    end
   end
 
   context "with a successful discard_draft" do
     it "discards the draft in the publishing api" do
+      guide = create(:guide)
       publisher = double(:publisher)
       expect(Publisher).to receive(:new).with(content_model: guide).and_return publisher
       expect(publisher).to receive(:discard_draft)
@@ -30,6 +28,7 @@ RSpec.describe "discarding guides", type: :feature do
 
   context "with an unsuccessful discard_draft" do
     it "does not discard the draft" do
+      guide = create(:guide)
       discard_draft_response = Publisher::DiscardDraftResponse.new(
         success: false,
         errors: "An error occurred",
