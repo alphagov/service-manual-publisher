04333f9c9e789fd7f1b6251a37711c359a31ebdf
Henry Turner <henry.turner@digital.cabinet-office.gov.uk>
2016-04-29 07:44:58 +0000

    Move loading of edition in SearchIndexer
    We only need to load the edition when indexing new content. For deleting guides from the index we don't need it.

--- app/models/search_indexer.rb
+++ app/models/search_indexer.rb
@@ -1,29 +1,30 @@
 class SearchIndexer
   def initialize(guide, rummager_index: RUMMAGER_INDEX)
     @guide = guide
-    @edition = guide.latest_published_edition
     @rummager_index = rummager_index
   end
 
   def index
+    edition = guide.latest_published_edition
+
     rummager_index.add_batch([{
       "format":            "service_manual_guide",
       "_type":             "service_manual_guide",
-      "description":       @edition.description,
-      "indexable_content": @edition.body,
-      "title":             @edition.title,
-      "link":              @guide.slug,
+      "description":       edition.description,
+      "indexable_content": edition.body,
+      "title":             edition.title,
+      "link":              guide.slug,
       "manual":            "service-manual",
       "organisations":     ["government-digital-service"],
     }])
   end
 
   def delete
-    rummager_index.delete(@guide.slug)
+    rummager_index.delete(guide.slug)
   end
 
 private
 
-  attr_reader :rummager_index
+  attr_reader :guide, :rummager_index
 
 end
