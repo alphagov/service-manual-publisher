2ee3474b20de5f63800015b9ff27dde1c1cbfa27
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-06-06 14:06:23 +0000

    Set x_govuk_authenticated_user header...
    https://trello.com/c/6nDCqZXi/729-user-id-into-event-log
    The publishing API event log should have a reference to the authenticated user
    so ensure that the appropriate GdsApi::GovukHeaders header is set to meet expectations
    of the publishing API event logging.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -12,5 +12,13 @@ RSpec.describe ApplicationController do
       expect(controller).to receive(:require_signin_permission!).and_return(true)
       get :index
     end
+
+    it "should set the authenticated user uid as a GdsApi::GovukHeader" do
+      user = double(:user, uid: "12345-67890", remotely_signed_out?: false, has_permission?: true)
+      login_as(user)
+
+      get :index
+      expect(GdsApi::GovukHeaders.headers[:x_govuk_authenticated_user]).to eq("12345-67890")
+    end
   end
 end
