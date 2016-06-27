39f0436a6ad02decad6c982104432c0ff7a7bc49
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2016-06-27 11:30:31 +0100

    Move select options into slug migration helper
    Select options are a view responsibility

---
+++ spec/helpers/slug_migrations_helper_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe SlugMigrationsHelper, '#select_options', type: :helper do
+  it 'should return all available slugs' do
+    guide = create(:published_guide)
+    topic = create(:topic)
+
+    expect(helper.select_options).to eq(
+      "Other" => ["/service-manual"],
+      "Topics" => [[topic.path, topic.path]],
+      "Guides" => [[guide.slug, guide.slug]]
+    )
+  end
+end
