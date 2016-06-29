f2f9b99663525fb77bcd951ef8bad3434af7201b
Henry Turner <henry.turner@digital.cabinet-office.gov.uk>
2016-06-29 07:47:44 +0000

    Save and publish the standard when publising a point
    Because the draft of the standard can be overwritten (by saving a different point) before it is published we need to save and publish the standard when a point has just been published.

--- app/forms/point_form.rb
+++ app/forms/point_form.rb
@@ -6,16 +6,4 @@ class PointForm < BaseGuideForm
   def slug_prefix
     "/service-manual/service-standard"
   end
-
-private
-
-  def save_draft_to_publishing_api
-    super
-
-    service_standard_for_publication = ServiceStandardPresenter.new(Point.all)
-    PUBLISHING_API.put_content(
-      service_standard_for_publication.content_id,
-      service_standard_for_publication.content_payload
-    )
-  end
 end
