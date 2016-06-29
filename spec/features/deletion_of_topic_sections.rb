82476ef2f10258d2edb51f31de0b7b90d00baa9f
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2016-06-29 15:58:33 +0100

    Don't use let in tests

--- spec/features/deletion_of_topic_sections.rb
+++ spec/features/deletion_of_topic_sections.rb
@@ -1,29 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Deletion of topic sections", type: :feature do
-  let(:delete_button_selector) { '.js-delete-list-group-item' }
-  let(:topic) { create(:topic, :with_some_guides) }
-
   it "lets you delete empty topic sections" do
-    empty_section = topic.topic_sections.create!(
-      title: "Empty Group",
-      description: "Empty group description",
-    )
+    topic = create(:topic)
+    section_without_guides = topic.topic_sections.create!(title: "Empty Group")
 
     visit edit_topic_path(topic)
 
-    within_topic_section(empty_section.title) do
-      expect(page).to have_css delete_button_selector
+    within_topic_section(section_without_guides.title) do
+      expect(page).to have_css '.js-delete-list-group-item'
     end
   end
 
   it "does not let you delete topic sections that contain guides" do
-    visit edit_topic_path(topic)
-
+    topic = create(:topic, :with_some_guides)
     section_with_guides = topic.topic_sections[0]
 
+    visit edit_topic_path(topic)
+
     within_topic_section(section_with_guides.title) do
-      expect(page).not_to have_css delete_button_selector
+      expect(page).not_to have_css '.js-delete-list-group-item'
     end
   end
 end
