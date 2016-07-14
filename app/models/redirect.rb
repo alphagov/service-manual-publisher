c572376b7740f0dc2ec476b91810fb8a1b0a4c1a
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2016-07-14 14:08:22 +0100

    Use a better method for friendly names in redirect validation errors

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -3,13 +3,4 @@ class Redirect < ActiveRecord::Base
 
   validates :old_path, presence: true
   validates :new_path, presence: true
-
-  HUMANIZED_ATTRIBUTES = {
-    old_path: 'Redirect source',
-    new_path: 'Redirect destination'
-  }.freeze
-
-  def self.human_attribute_name(attr, options = {})
-    HUMANIZED_ATTRIBUTES[attr.to_sym] || super
-  end
 end
