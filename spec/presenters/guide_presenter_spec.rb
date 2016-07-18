d28dfb94d138ad415af12d4f3776ed0e53e22b6c
Henry Turner <henry.turner@digital.cabinet-office.gov.uk>
2016-07-18 11:44:46 +0000

    Reword tests that assert we're not sending public_updated_at

--- spec/presenters/guide_presenter_spec.rb
+++ spec/presenters/guide_presenter_spec.rb
@@ -41,8 +41,13 @@ RSpec.describe GuidePresenter do
       )
     end
 
-    it "doesn't contain public_updated_at so that it represents the published at time" \
-     " on the frontend" do
+    it "omits public_updated_at" do
+      # If the payload includes public_updated_at then the user facing timestamp on the
+      # frontend will reflect the time the draft was saved rather than the time it was
+      # published. For the service manual we want to display the published time to the user.
+      #
+      # https://github.com/alphagov/content-store/blob/master/doc/content_item_fields.md#public_updated_at
+      #
       expect(presenter.content_payload).to_not have_key(:public_updated_at)
     end
 
