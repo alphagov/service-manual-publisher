7b8627e07931cae5c71b3952890846451a853d5e
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2016-08-04 11:05:57 +0100

    Remove unused topic section scope

--- app/models/topic_section.rb
+++ app/models/topic_section.rb
@@ -4,8 +4,4 @@ class TopicSection < ActiveRecord::Base
 
   has_many :topic_section_guides, -> { order(position: :asc) }, dependent: :destroy
   accepts_nested_attributes_for :topic_section_guides
-
-  scope :belonging_to_topic, ->(topic_id) {
-    where(topic_id: topic_id)
-  }
 end
