762c6e8c47d94f2e0fe35aa508bf923898fe034c
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2016-08-19 16:07:07 +0100

    Fix a couple of n+1 queries within publisher

--- app/controllers/editions_controller.rb
+++ app/controllers/editions_controller.rb
@@ -1,6 +1,6 @@
 class EditionsController < ApplicationController
   def index
-    @guide = Guide.includes(editions: :author).find(params[:guide_id])
+    @guide = Guide.includes(editions: [:author, :created_by]).find(params[:guide_id])
     @current_edition = if params[:current_edition].present?
                          @guide.editions.find(params[:current_edition])
                        else
