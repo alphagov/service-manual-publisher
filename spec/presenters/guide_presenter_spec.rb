4973f342a2553586ccfc2924cf9ae256d7c655ae
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2016-08-26 14:00:26 +0100

    Clean up the guide presenter spec
    Remove confusing usage of let and overriding of lets in favour of localised setup of what's needed for each individual test.

--- spec/presenters/guide_presenter_spec.rb
+++ spec/presenters/guide_presenter_spec.rb
@@ -1,49 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe GuidePresenter do
-  let(:topic) do
-    create(:topic,
-      title: "Parent Topic",
-      content_id: "4ac0bacf-0062-47fd-b1ce-852a95c25e20"
-    )
-  end
-
-  let(:guide) do
-    create(:guide,
-      content_id: "220169e2-ae6f-44f5-8459-5a79e0a78537",
-      edition: {
-        title: "The Title",
-        state: "draft",
-        phase: "beta",
-        description: "Description",
-        update_type: "major",
-        body: "# Heading",
-        created_at: "2016-06-28T14:16:21Z".to_time,
-        updated_at: "2016-06-28T14:16:21Z".to_time,
-        change_note: "Add a new guide 'The Title'",
-        reason_for_change: "We added this guide so we can test the presenter"
-      },
-      slug: '/service-manual/test-topic/the-title',
-      topic: topic
-    )
-  end
-
-  let(:edition) { guide.latest_edition }
-
-  let(:presenter) { described_class.new(guide, edition) }
-
   describe "#content_payload" do
     it "conforms to the schema" do
+      guide = create(:guide)
+      presenter = described_class.new(guide, guide.latest_edition)
+
       expect(presenter.content_payload).to be_valid_against_schema('service_manual_guide')
     end
 
     describe "common service manual draft payload" do
-      let(:payload) { presenter.content_payload }
+      let(:payload) {
+        guide = create(:guide)
+        presenter = described_class.new(guide, guide.latest_edition)
+        presenter.content_payload
+      }
 
       include_examples "common service manual draft payload"
     end
 
     it "exports all necessary metadata" do
+      guide = create(:guide,
+        edition: {
+          phase: "beta",
+          update_type: "major",
+          description: "Description"
+        },
+        slug: '/service-manual/test-topic/the-title'
+      )
+      presenter = described_class.new(guide, guide.latest_edition)
+
       expect(presenter.content_payload).to include(
         description: "Description",
         update_type: "major",
@@ -55,6 +41,15 @@ RSpec.describe GuidePresenter do
     end
 
     it "includes the change history for the edition" do
+      guide = create(:guide,
+        edition: {
+          created_at: "2016-06-28T14:16:21Z".to_time,
+          change_note: "Add a new guide 'The Title'",
+          reason_for_change: "We added this guide so we can test the presenter"
+        }
+      )
+      presenter = described_class.new(guide, guide.latest_edition)
+
       expect(presenter.content_payload[:details]).to include(
         change_history: [
           {
@@ -67,19 +62,25 @@ RSpec.describe GuidePresenter do
     end
 
     it "includes the latest change note for email notifications" do
+      guide = create(:guide, edition: { change_note: "Add a new guide" })
+      presenter = described_class.new(guide, guide.latest_edition)
+
       expect(presenter.content_payload[:details]).to include(
-        change_note: "Add a new guide 'The Title'"
+        change_note: "Add a new guide"
       )
     end
 
     it "omits the content owner if the edition doesn't have one" do
-      edition.content_owner = nil
+      guide = create(:guide, edition: { content_owner: nil })
+      presenter = described_class.new(guide, guide.latest_edition)
 
       expect(presenter.content_payload[:details][:content_owner]).to be_blank
     end
 
     it "includes h2 links for the sidebar" do
-      edition.body = "## Header 1 \n\n### Subheader \n\n## Header 2\n\ntext"
+      guide = create(:guide, body:  "## Header 1 \n\n### Subheader \n\n## Header 2\n\ntext")
+      presenter = described_class.new(guide, guide.latest_edition)
+
       expect(presenter.content_payload[:details][:header_links]).to match_array([
         { title: "Header 1", href: "#header-1" },
         { title: "Header 2", href: "#header-2" }
@@ -87,12 +88,16 @@ RSpec.describe GuidePresenter do
     end
 
     it "renders body to HTML" do
-      edition.body = "__look at me__"
+      guide = create(:guide, body: "__look at me__")
+      presenter = described_class.new(guide, guide.latest_edition)
+
       expect(presenter.content_payload[:details][:body]).to include("<strong>look at me</strong>")
     end
 
     it "exports the title" do
-      edition.title = "Agile Process"
+      guide = create(:guide, title: "Agile Process")
+      presenter = described_class.new(guide, guide.latest_edition)
+
       expect(presenter.content_payload[:title]).to eq("Agile Process")
     end
 
@@ -109,45 +114,62 @@ RSpec.describe GuidePresenter do
   end
 
   describe '#links_payload' do
-    it "includes an organisation" do
-      expect(
-        presenter.links_payload[:links][:organisations]
-      ).to match_array([an_instance_of(String)])
-    end
+    context 'for all types of guide' do
+      it "includes the GDS Organisation ID" do
+        guide = create(:guide)
+        presenter = described_class.new(guide, guide.latest_edition)
 
-    it 'includes a reference to the service manual topic' do
-      expect(
-        presenter.links_payload[:links][:service_manual_topics]
-      ).to match_array(["4ac0bacf-0062-47fd-b1ce-852a95c25e20"])
-    end
+        expect(presenter.links_payload[:links][:organisations])
+          .to eq ['af07d5a5-df63-4ddc-9383-6a666845ebe9']
+      end
 
-    it 'returns the content owner if present' do
-      edition.content_owner = build(:guide_community)
-      expect(presenter.links_payload[:links][:content_owners]).to eq(
-        [edition.content_owner.content_id]
-      )
+      it 'includes a reference to the service manual topic' do
+        topic = create(:topic, content_id: '4ac0bacf-0062-47fd-b1ce-852a95c25e20')
+        guide = create(:guide, topic: topic)
+        presenter = described_class.new(guide, guide.latest_edition)
+
+        expect(presenter.links_payload[:links][:service_manual_topics])
+          .to eq ["4ac0bacf-0062-47fd-b1ce-852a95c25e20"]
+      end
+
+      it 'returns the content owner if present' do
+        owner = create(:guide_community, content_id: 'c5eb647c-7943-49dd-8362-1920d330696f')
+        guide = create(:guide, edition: { content_owner_id: owner.id })
+        presenter = described_class.new(guide, guide.latest_edition)
+
+        expect(presenter.links_payload[:links][:content_owners])
+          .to eq(['c5eb647c-7943-49dd-8362-1920d330696f'])
+      end
     end
 
     context 'for a guide community' do
-      let(:guide) { create(:guide_community) }
-
       it "doesn't include content owners" do
+        guide = create(:guide_community)
+        presenter = described_class.new(guide, guide.latest_edition)
+
         expect(presenter.links_payload[:links]).not_to have_key(:content_owners)
       end
     end
 
     context 'for a point' do
-      let(:guide) { create(:point) }
-
       it "doesn't include a link to a topic" do
-        expect(presenter.links_payload[:links]).not_to have_key(:service_manual_topics)
+        guide = create(:point)
+        presenter = described_class.new(guide, guide.latest_edition)
+
+        expect(presenter.links_payload[:links]).not_to have_key :service_manual_topics
       end
 
       it "doesn't include content owners" do
-        expect(presenter.links_payload[:links]).not_to have_key(:content_owners)
+        guide = create(:point)
+        presenter = described_class.new(guide, guide.latest_edition)
+
+        expect(presenter.links_payload[:links]).not_to have_key :content_owners
       end
 
       it "includes the service standard as a parent in the links" do
+        guide = create(:point)
+        presenter = described_class.new(guide, guide.latest_edition)
+
         expect(presenter.links_payload[:links]).to include(
           parent: ["00f693d4-866a-4fe6-a8d6-09cd7db8980b"]
         )
