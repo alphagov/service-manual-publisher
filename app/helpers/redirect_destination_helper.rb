53cc85f54e63d6af275a2fc95071ea64facc361c
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2016-11-21 14:03:24 +0000

    Allow redirecting to topic sections (by anchor)
    Break topics down to include ‘anchor’ link options for all sub topics as well as a link option for the topic itself.
    
    This allows us to redirect old parts of the manual to a specific sub topic within a topic, even though it has no page of its own.

--- app/helpers/redirect_destination_helper.rb
+++ app/helpers/redirect_destination_helper.rb
@@ -14,6 +14,15 @@ private
   end
 
   def topic_select_options
-    Topic.order(:path).pluck(:path)
+    Topic.order(:path)
+      .flat_map { |topic| paths_for_topic_and_its_sections(topic) }
+  end
+
+  def paths_for_topic_and_its_sections(topic)
+    topic_sections = topic.topic_sections.order(:title).where("title <> ''").map do |section|
+      ["#{topic.path} → #{section.title}", "#{topic.path}##{section.title.parameterize}"]
+    end
+
+    [topic.path, *topic_sections]
   end
 end
