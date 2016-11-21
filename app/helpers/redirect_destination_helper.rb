2970bf81fedf73144afad10805ee95b5e71ee3de
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2016-11-21 10:24:33 +0000

    Refactor redirect destination helper
    Refactor both helper and test into smaller methods that do and test less. Also test that we’re ordering by slugs.

--- app/helpers/redirect_destination_helper.rb
+++ app/helpers/redirect_destination_helper.rb
@@ -1,16 +1,19 @@
 module RedirectDestinationHelper
   def redirect_destination_select_options
-    guide_select_options = Guide
-      .live
-      .order(:slug).pluck(:slug)
-
-    topic_select_options = Topic
-      .order(:path).pluck(:path)
-
     {
       "Other" => ["/service-manual", "/service-manual/service-standard"],
       "Topics" => topic_select_options,
       "Guides" => guide_select_options,
     }
+  end
+
+private
+
+  def guide_select_options
+    Guide.live.order(:slug).pluck(:slug)
+  end
+
+  def topic_select_options
+    Topic.order(:path).pluck(:path)
   end
 end
--- spec/helpers/redirect_destination_helper_spec.rb
+++ spec/helpers/redirect_destination_helper_spec.rb
@@ -1,15 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe RedirectDestinationHelper, '#redirect_destination_select_options', type: :helper do
-  it 'should return all available slugs' do
-    topic = create(:topic)
-    create(:guide, :has_been_unpublished, topic: topic)
-    guide = create(:guide, :with_published_edition, topic: topic)
+  it 'should include all published guides ordered by slug' do
+    create(:guide, :with_published_edition,
+      slug: "/service-manual/agile-delivery/team-wall"
+    )
+    create(:guide, :with_published_edition,
+      slug: "/service-manual/agile-delivery/core-principles-agile"
+    )
 
-    expect(helper.redirect_destination_select_options).to match(
+    create(:guide, :has_been_unpublished)
+    create(:guide, :with_draft_edition)
+
+    expect(helper.redirect_destination_select_options).to include(
+      "Guides" => [
+        "/service-manual/agile-delivery/core-principles-agile",
+        "/service-manual/agile-delivery/team-wall"
+      ]
+    )
+  end
+
+  it 'should include the homepage and the service standard' do
+    expect(helper.redirect_destination_select_options).to include(
       "Other" => ["/service-manual", "/service-manual/service-standard"],
-      "Topics" => include(topic.path),
-      "Guides" => [guide.slug]
+    )
+  end
+
+  it 'should include all topics' do
+    create(:topic, path: "/service-manual/agile-delivery")
+
+    expect(helper.redirect_destination_select_options).to include(
+      "Topics" => ["/service-manual/agile-delivery"]
     )
   end
 end
