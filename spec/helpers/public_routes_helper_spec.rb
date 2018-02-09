2bd166440d84ec6ae84b81b387c66ec11470734e
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-02-09 13:26:19 +0000

    Use the new external_url_for method
    Where external URLs are required.

--- spec/helpers/public_routes_helper_spec.rb
+++ spec/helpers/public_routes_helper_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe PublicRoutesHelper do
   describe "#document_preview_url" do
     it "concatenates frontend host and document slug" do
-      expect(Plek).to receive(:find).once.and_return("http://frontend-host.dev.gov.uk")
+      expect_any_instance_of(Plek).to receive(:external_url_for).once.and_return("http://frontend-host.dev.gov.uk")
       guide = Guide.new(slug: "/guide/slug")
 
       expect(document_preview_url(guide.slug)).to eq "http://frontend-host.dev.gov.uk/guide/slug"
