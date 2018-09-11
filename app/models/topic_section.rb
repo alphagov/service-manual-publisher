25ef1117c025d523b0554edbd1ca56b0cb32fccb
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-09-11 17:55:29 +0100

    Reorder has_many to be dependent on each other

--- app/models/topic_section.rb
+++ app/models/topic_section.rb
@@ -1,7 +1,8 @@
 class TopicSection < ApplicationRecord
   belongs_to :topic
-  has_many :guides, through: :topic_section_guides
 
   has_many :topic_section_guides, -> { order(position: :asc) }, dependent: :destroy
   accepts_nested_attributes_for :topic_section_guides
+
+  has_many :guides, through: :topic_section_guides
 end
