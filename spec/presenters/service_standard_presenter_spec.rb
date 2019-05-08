549340b17dd15fe57b9e1f1e487e87700d33f0a7
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-05-08 10:27:01 +0100

    Fix pre-2019 service standard URL

--- spec/presenters/service_standard_presenter_spec.rb
+++ spec/presenters/service_standard_presenter_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe ServiceStandardPresenter, "#content_payload" do
       update_type: 'major',
       description: "The Service Standard helps teams to create and run great public services.",
       details: {
-        body: '<p>All public facing transactional services must meet the standard. It’s used by departments and the Government Digital Service to check whether a service is good enough for public use.</p><p>Check whether you need to use <a href="/service-manual/service-assessments/pre-2019-digital-service-standard"> the previous version of the Service Standard</a>.</p>'
+        body: '<p>All public facing transactional services must meet the standard. It’s used by departments and the Government Digital Service to check whether a service is good enough for public use.</p><p>Check whether you need to use <a href="/service-manual/service-assessments/pre-july-2019-digital-service-standard"> the previous version of the Service Standard</a>.</p>'
       }
     }
 
