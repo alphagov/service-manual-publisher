d36e9ae9d91469ca98a9a7e5f4ca98c0d242172d
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2020-10-15 10:01:23 +0100

    Add error handler for Notify BadRequestError

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_authenticated_user_header
 
+  rescue_from Notifications::Client::BadRequestError, with: :notify_bad_request
+
   def preview_content_model_url(content_model)
     [Plek.new.external_url_for("draft-origin"), content_model.slug].join("")
   end
@@ -16,5 +18,9 @@ class ApplicationController < ActionController::Base
     if current_user && GdsApi::GovukHeaders.headers[:x_govuk_authenticated_user].nil?
       GdsApi::GovukHeaders.set_header(:x_govuk_authenticated_user, current_user.uid)
     end
+  end
+
+  def notify_bad_request(_exception)
+    render plain: "Error: One or more recipients not in GOV.UK Notify team (code: 400)", status: :bad_request
   end
 end
