4d968c8bdb066c1ebd72474408d7cccbe4428c8e
Murilo Dal Ri <murilo.dalri@digital.cabinet-office.gov.uk>
2020-12-08 12:12:40 +0000

    Add 200 ok healthcheck test

--- spec/requests/healthcheck_spec.rb
+++ spec/requests/healthcheck_spec.rb
@@ -1,6 +1,13 @@
 RSpec.describe "/healthcheck" do
-  it "returns database connection status" do
+  before do
     get "/healthcheck"
+  end
+
+  it "returns a 200 HTTP status" do
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "returns database connection status" do
     json = JSON.parse(response.body)
 
     expect(json["checks"]).to include("database_connectivity")
