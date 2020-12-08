259d40fdd70bb4a1ac2ffc0e84f56ff0570f4c96
Murilo Dal Ri <murilo.dalri@digital.cabinet-office.gov.uk>
2020-12-08 12:19:05 +0000

    Remove redundant test

--- spec/requests/utility_pages_spec.rb
+++ spec/requests/utility_pages_spec.rb
@@ -1,16 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "utility pages", type: :request do
-  it "should respond with 'OK' to /healthcheck" do
-    get "/healthcheck"
-
-    expect(response.status).to eq(200)
-    expect(response.body).to eq("OK")
-  end
-
   it "should respond with 200 to /style-guide" do
     get "/style-guide"
 
-    expect(response.status).to eq(200)
+    expect(response).to have_http_status(:ok)
   end
 end
