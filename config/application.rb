e29863311af3e4acabdcc032187798c8119397b5
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2021-07-16 12:06:22 +0100

    Apply rails app:update changes

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,19 @@
 require_relative "boot"
 
 require "rails"
-
+# Pick the frameworks you want:
 require "active_model/railtie"
+# require "active_job/railtie"
 require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
+# require "action_cable/engine"
 require "sprockets/railtie"
 require "action_mailer/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -17,6 +23,7 @@ module ServiceManualPublisher
   GDS_ORGANISATION_CONTENT_ID = "af07d5a5-df63-4ddc-9383-6a666845ebe9".freeze
 
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
