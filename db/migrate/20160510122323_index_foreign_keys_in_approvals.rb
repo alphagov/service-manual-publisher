672475b3d238ab727814a4672079674a68db31d5
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-07-26 16:57:47 +0100

    Prevent attempting to create indexes if they already exist
    When running the migrations more than once (e.g. when running
    `make service-manual-publisher` in govuk-docker when the container
    is already built), the command fails with several errors along the
    lines of:
    
    > ArgumentError: Index name 'index_editions_on_author_id' on table
    > 'editions' already exists
    
    We can solve this by coding defensively, only creating the indexes
    if they have not been created yet.

--- db/migrate/20160510122323_index_foreign_keys_in_approvals.rb
+++ db/migrate/20160510122323_index_foreign_keys_in_approvals.rb
@@ -1,6 +1,6 @@
 class IndexForeignKeysInApprovals < ActiveRecord::Migration[5.2]
   def change
-    add_index :approvals, :edition_id
-    add_index :approvals, :user_id
+    add_index :approvals, :edition_id unless index_exists?(:approvals, :edition_id)
+    add_index :approvals, :user_id unless index_exists?(:editions, :user_id)
   end
 end
--- db/migrate/20160510122324_index_foreign_keys_in_editions.rb
+++ db/migrate/20160510122324_index_foreign_keys_in_editions.rb
@@ -1,7 +1,7 @@
 class IndexForeignKeysInEditions < ActiveRecord::Migration[5.2]
   def change
-    add_index :editions, :author_id
-    add_index :editions, :content_owner_id
-    add_index :editions, :guide_id
+    add_index :editions, :author_id unless index_exists?(:editions, :author_id)
+    add_index :editions, :content_owner_id unless index_exists?(:editions, :content_owner_id)
+    add_index :editions, :guide_id unless index_exists?(:editions, :guide_id)
   end
 end
