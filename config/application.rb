106c700813effe8f2f56af0df84ac2cead70ffe6
Callum Knights <callum.knights@digital.cabinet-office.gov.uk>
2022-03-09 20:20:24 +0000

    Run `rails app:update`

--- config/application.rb
+++ config/application.rb
@@ -3,16 +3,15 @@ require_relative "boot"
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-# require "active_job/railtie"
+#  require "active_job/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
+require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
 require "action_view/railtie"
 # require "action_cable/engine"
-require "sprockets/railtie"
-require "action_mailer/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -25,9 +24,11 @@ module ServiceManualPublisher
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
 
     config.action_mailer.default_url_options = { host: Plek.new.external_url_for("service-manual-publisher") }
 
@@ -40,5 +41,8 @@ module ServiceManualPublisher
     config.active_record.belongs_to_required_by_default = false
 
     ActiveSupport.to_time_preserves_timezone = false
+
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
