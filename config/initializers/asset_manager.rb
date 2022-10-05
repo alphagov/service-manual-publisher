78664f388c45e4838f4f399146e7e0baa0e81aa9
Rebecca Pearce <rebecca.pearce@digital.cabinet-office.gov.uk>
2022-10-05 14:32:04 +0100

    Change instances of Plek.current to Plek.new

--- config/initializers/asset_manager.rb
+++ config/initializers/asset_manager.rb
@@ -1,6 +1,6 @@
 require "gds_api/asset_manager"
 
 ASSET_API = GdsApi::AssetManager.new(
-  Plek.current.find("asset-manager"),
+  Plek.new.find("asset-manager"),
   bearer_token: ENV["ASSET_MANAGER_BEARER_TOKEN"] || "12345678", # test and development environments accept any value
 )
